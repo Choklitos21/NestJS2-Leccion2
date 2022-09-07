ac65a17ee858a631f66c3dbcf5be79351f404e02
DiegoMorales <diego.mm09@hotmail.com>
2022-09-07 15:16:05 -0500

    last update

--- src/auth/dto/login-user.dto.ts
+++ src/auth/dto/login-user.dto.ts
@@ -1,13 +1,12 @@
 import {ApiProperty} from "@nestjs/swagger";
-import {IsNotEmpty, IsString, MaxLength, MinLength} from "class-validator";
+import {IsEmail, IsNotEmpty, IsString, MaxLength, MinLength} from "class-validator";
 
 export class LoginUserDto {
 
     @ApiProperty({ type: String })
     @IsNotEmpty()
-    @IsString()
-    @MinLength(1)
-    full_name: string;
+    @IsEmail()
+    email: string;
 
     @ApiProperty({ type: String })
     @IsNotEmpty()
